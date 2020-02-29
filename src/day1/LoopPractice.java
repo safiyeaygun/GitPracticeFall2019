575f1f3f0a2385ba6854c99c8d17237b90529d36
safiyeaygun <safiyeaygun88@gmail.com>
2020-02-29 15:53:10 -0500

    update

--- src/day1/LoopPractice.java
+++ src/day1/LoopPractice.java
@@ -4,7 +4,15 @@ public class LoopPractice {
         //some random code
         for (int i=0; i <10; i+=2){
             System.out.println("Counter :: "+i);
+            System.out.println("#####");
+            System.out.println("git is fun");
+
+
         }
+
+
+
+
     }
 }
 
